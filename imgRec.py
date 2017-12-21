10c36eb7aca25a5f12fcaf5a4cea60f00e385650
ajackows <atj236@nyu.edu>
2017-12-21 13:08:45 -0500

    Update imgRec.py

--- imgRec.py
+++ imgRec.py
@@ -26,23 +26,18 @@ from utils import visualization_utils as vis_util
 
 # # Model preparation 
 
-# ## Variables
-# 
-# Any model exported using the `export_inference_graph.py` tool can be loaded here simply by changing `PATH_TO_CKPT` to point to a new .pb file.  
-# 
-# By default we use an "SSD with Mobilenet" 
 
-# What model to download.
+# What model to download, using the custom NASA image model
 MODEL_NAME = 'NASA_inference_graph'
-#MODEL_FILE = MODEL_NAME + '.tar.gz'
-#DOWNLOAD_BASE = 'http://download.tensorflow.org/models/object_detection/'
+
 
 # Path to frozen detection graph. This is the actual model that is used for the object detection.
 PATH_TO_CKPT = MODEL_NAME + '/frozen_inference_graph.pb'
 
-# List of the strings that is used to add correct label for each box.
+# Set path to the label map so the program knows thobject name (NASA)
 PATH_TO_LABELS = os.path.join('training', 'object-detection.pbtxt')
 
+# only one classification object, the NASA meatball
 NUM_CLASSES = 1
 
 # ## Load a (frozen) Tensorflow model into memory.
@@ -57,31 +52,32 @@ with detection_graph.as_default():
 
 
 # ## Loading label map
-# Label maps map indices to category names, so that when our convolution network predicts `5`, we know that this corresponds to `airplane`.  Here we use internal utility functions, but anything that returns a dictionary mapping integers to appropriate string labels would be fine
 
 label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
 categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
 category_index = label_map_util.create_category_index(categories)
 
 
-# Size, in inches, of the output images.
-IMAGE_SIZE = (12, 8)
-
+#NOTE parts of this section were copied from the Tensor Flow Example notebook
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
     while True:
+      #get image from webcam
       ret, image_np = cap.read()
+      
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
       image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+      
       # Each box represents a part of the image where a particular object was detected.
       boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
-      # Each score represent how level of confidence for each of the objects.
       
+      # Each score represent how level of confidence for each of the objects.
       # Score is shown on the result image, together with the class label.
       scores = detection_graph.get_tensor_by_name('detection_scores:0')
       classes = detection_graph.get_tensor_by_name('detection_classes:0')
       num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+      
       # Actual detection.
       (boxes, scores, classes, num_detections) = sess.run(
           [boxes, scores, classes, num_detections],
@@ -97,6 +93,7 @@ with detection_graph.as_default():
           line_thickness=8)
 
       cv2.imshow('object detection', cv2.resize(image_np, (800,600)))
+      #use 'q' key to close program
       if cv2.waitKey(25) & 0xFF == ord('q'):
         cv2.destroyAllWindows()
         break
